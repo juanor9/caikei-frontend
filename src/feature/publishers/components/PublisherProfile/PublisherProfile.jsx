f47aeb3f5fdff7938ce38e9b6ec846e1b6a5a592
Juan Camilo Orjuela <orjuela9@gmail.com>
2023-01-31 19:46:08 -0500

    set deactivate publisher button

--- src/feature/publishers/components/PublisherProfile/PublisherProfile.jsx
+++ src/feature/publishers/components/PublisherProfile/PublisherProfile.jsx
@@ -36,9 +36,7 @@ const PublisherProfile = () => {
     event.preventDefault();
 
     try {
-      dispatch(
-        updatePublisher({ ...form, publisherId: publisher }),
-      );
+      dispatch(updatePublisher({ ...form, publisherId: publisher }));
       setInfoModal(false);
     } catch (error) {
       throw new Error(error);
@@ -63,6 +61,16 @@ const PublisherProfile = () => {
     }
   };
 
+  // deactivate publisher
+  const handleClickDeactivate = () => {
+    try {
+      const deactivate = { isActive: false };
+      const data = { deactivate, publisherId: publisher };
+      dispatch(updatePublisher(data));
+    } catch (error) {
+      throw new Error(error);
+    }
+  };
   // if data is lost, ask for data to server
   useEffect(() => {
     if (!email) {
@@ -92,12 +100,9 @@ const PublisherProfile = () => {
         {publisher ? (
           <>
             <article className="publisher-profile__logo">
-
               <div>
                 <h3>Logo</h3>
-                <figure
-                  className="publisher-profile__logo-figure"
-                >
+                <figure className="publisher-profile__logo-figure">
                   <img
                     src={logo}
                     alt={`${name}-logo`}
@@ -118,16 +123,13 @@ const PublisherProfile = () => {
                 <b>Nombre: </b>
                 {name}
               </div>
-              {publisherIds
-                ? (
-                  <div className="publisher-profile__info">
-                    <b>Documento de identidad: </b>
-                    {publisherIds[publisherIds.length - 1].type}
-                    {' '}
-                    {publisherIds[publisherIds.length - 1].number}
-                  </div>
-                )
-                : null}
+              {publisherIds ? (
+                <div className="publisher-profile__info">
+                  <b>Documento de identidad: </b>
+                  {publisherIds[publisherIds.length - 1].type}{' '}
+                  {publisherIds[publisherIds.length - 1].number}
+                </div>
+              ) : null}
               <div className="publisher-profile__info">
                 <b>Correo electrónico: </b> {publisherEmail}
               </div>
@@ -140,141 +142,136 @@ const PublisherProfile = () => {
               <button
                 type="button"
                 className="publisher-profile__button"
-                onClick={() => { setInfoModal(true); }}
-              >Editar información
+                onClick={() => {
+                  setInfoModal(true);
+                }}
+              >
+                Editar información
+              </button>
+              <button
+                type="button"
+                className="publisher-profile__deactivate"
+                onClick={handleClickDeactivate}
+              >
+                Desactivar editorial
               </button>
             </article>
-
           </>
         ) : (
           <div className="publisher-profile__info">
             <p>Aún no tienes una editorial registrada.</p>
-            <Link to="/publisher/register" className="publisher-profile__button">
+            <Link
+              to="/publisher/register"
+              className="publisher-profile__button"
+            >
               <FontAwesomeIcon icon={faPlus} />
               Registra una editorial
             </Link>
           </div>
         )}
-        {infoModal === true
-          ? (
-            <Modal
-              className="publisher-profile__modal"
-              modalFunction={setInfoModal}
+        {infoModal === true ? (
+          <Modal
+            className="publisher-profile__modal"
+            modalFunction={setInfoModal}
+          >
+            <form
+              action=""
+              className="publisher-profile__modal-form"
+              onSubmit={handleSubmit}
             >
+              <h3>Actualizar datos</h3>
+              <label className="publisher-profile__modal-label" htmlFor="name">
+                Nombre
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  className="publisher-profile__modal-input"
+                  defaultValue={name}
+                  onChange={handleChange}
+                />
+              </label>
+              <label htmlFor="email" className="publisher-profile__modal-label">
+                Correo electrónico
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="publisher-profile__modal-input"
+                  defaultValue={publisherEmail}
+                  onChange={handleChange}
+                />
+              </label>
+              <label
+                htmlFor="address"
+                className="publisher-profile__modal-label"
+              >
+                Dirección
+                <input
+                  type="text"
+                  name="address"
+                  id="address"
+                  className="publisher-profile__modal-input"
+                  defaultValue={address}
+                  onChange={handleChange}
+                />
+              </label>
+              <label htmlFor="phone" className="publisher-profile__modal-label">
+                Teléfono
+                <input
+                  type="text"
+                  name="phone"
+                  id="phone"
+                  className="publisher-profile__modal-input"
+                  defaultValue={phone}
+                  onChange={handleChange}
+                />
+              </label>
+              <button type="submit" className="publisher-profile__modal-button">
+                Actualizar
+              </button>
+            </form>
+          </Modal>
+        ) : null}
+        {logoModal === true ? (
+          <Modal modalFunction={setLogoModal}>
+            <>
+              <figure className="publisher-profile__logo-figure">
+                <img
+                  src={logo}
+                  alt={`${name}-logo`}
+                  id="logo"
+                  className="publisher-profile__logo-img"
+                />
+              </figure>
               <form
                 action=""
+                onSubmit={handleSubmitimage}
                 className="publisher-profile__modal-form"
-                onSubmit={handleSubmit}
               >
-                <h3>Actualizar datos</h3>
+                <h3>Cargar nuevo logo</h3>
                 <label
-                  className="publisher-profile__modal-label"
-                  htmlFor="name"
-                >
-                  Nombre
-                  <input
-                    type="text"
-                    id="name"
-                    name="name"
-                    className="publisher-profile__modal-input"
-                    defaultValue={name}
-                    onChange={handleChange}
-                  />
-                </label>
-                <label
-                  htmlFor="email"
+                  htmlFor="new-logo"
                   className="publisher-profile__modal-label"
                 >
-                  Correo electrónico
                   <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    className="publisher-profile__modal-input"
-                    defaultValue={publisherEmail}
-                    onChange={handleChange}
-                  />
-                </label>
-                <label
-                  htmlFor="address"
-                  className="publisher-profile__modal-label"
-                >
-                  Dirección
-                  <input
-                    type="text"
-                    name="address"
-                    id="address"
-                    className="publisher-profile__modal-input"
-                    defaultValue={address}
-                    onChange={handleChange}
-                  />
-                </label>
-                <label
-                  htmlFor="phone"
-                  className="publisher-profile__modal-label"
-                >
-                  Teléfono
-                  <input
-                    type="text"
-                    name="phone"
-                    id="phone"
-                    className="publisher-profile__modal-input"
-                    defaultValue={phone}
-                    onChange={handleChange}
+                    type="file"
+                    id="new-logo"
+                    name="new-logo"
+                    accept="image/*"
+                    onChange={handleChangeImage}
                   />
                 </label>
                 <button
                   type="submit"
                   className="publisher-profile__modal-button"
-                >Actualizar
+                >
+                  Actualizar logo
                 </button>
               </form>
-            </Modal>
-          )
-          : null}
-        {logoModal === true
-          ? (
-            <Modal modalFunction={setLogoModal}>
-              <>
-                <figure
-                  className="publisher-profile__logo-figure"
-                >
-                  <img
-                    src={logo}
-                    alt={`${name}-logo`}
-                    id="logo"
-                    className="publisher-profile__logo-img"
-                  />
-                </figure>
-                <form
-                  action=""
-                  onSubmit={handleSubmitimage}
-                  className="publisher-profile__modal-form"
-                >
-                  <h3>Cargar nuevo logo</h3>
-                  <label
-                    htmlFor="new-logo"
-                    className="publisher-profile__modal-label"
-                  >
-                    <input
-                      type="file"
-                      id="new-logo"
-                      name="new-logo"
-                      accept="image/*"
-                      onChange={handleChangeImage}
-                    />
-                  </label>
-                  <button
-                    type="submit"
-                    className="publisher-profile__modal-button"
-                  >Actualizar logo
-                  </button>
-                </form>
-              </>
-            </Modal>
-          )
-          : null}
-
+            </>
+          </Modal>
+        ) : null}
       </section>
     </>
   );
