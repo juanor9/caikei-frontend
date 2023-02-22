d0f0c66c29b90f733d5c2595c3c98ed897d2ecdb
Juan Camilo Orjuela <orjuela9@gmail.com>
2023-02-22 17:28:49 -0500

    Set full inventory by libraries

--- src/feature/movements/components/InventoryByBook/InventoryByBook.jsx
+++ src/feature/movements/components/InventoryByBook/InventoryByBook.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import './InventoryByBook.scss';
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
@@ -6,15 +5,13 @@ import { getBooksByFilter } from '../../../books/services/books';
 import getLibrariesByPublisher from '../../../libraries/services/allLibraries';
 
 const InventoryByBook = () => {
-  const dispatch = useDispatch();
-  const { publisher } = useSelector((state) => state.user.userData);
-  const { catalogue } = useSelector((state) => state.catalogue);
-  const { allLibraries } = useSelector((state) => state.allLibraries);
-  const publisherData = useSelector((state) => state.publisher.publisher);
-
-  const userToken = localStorage.getItem('login-token');
-
   const [fullInventory, setFullInventory] = useState([]);
+  const { allLibraries } = useSelector((state) => state.allLibraries);
+  const { catalogue } = useSelector((state) => state.catalogue);
+  const { publisher } = useSelector((state) => state.user.userData);
+  const dispatch = useDispatch();
+  const publisherData = useSelector((state) => state.publisher.publisher);
+  const userToken = localStorage.getItem('login-token');
 
   useEffect(() => {
     dispatch(getBooksByFilter({ publisher, userToken }));
@@ -57,13 +54,12 @@ const InventoryByBook = () => {
   return (
     <section className="by-book">
       <h3>Por libros</h3>
-      {/* {console.log(fullInventory)} */}
       {fullInventory && Array.isArray(fullInventory)
         ? fullInventory.map((book) => (
           <article key={book._id}>
             <figure className="by-book__book-fig">
               <img src={book.cover} alt={book.title} className="by-book__book-img" />
-              <figcaption>{book.title}</figcaption>
+              <figcaption><h4>{book.title}</h4></figcaption>
             </figure>
             <div />
             {book.$inventory && Array.isArray(book.$inventory) && (
--- src/feature/movements/components/InventoryByLibrary/InventoryByLibrary.jsx
+++ src/feature/movements/components/InventoryByLibrary/InventoryByLibrary.jsx
@@ -1,15 +1,108 @@
-/* eslint-disable no-unused-vars */
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getBooksByFilter } from '../../../books/services/books';
+import getLibrariesByPublisher from '../../../libraries/services/allLibraries';
+import './InventoryByLibrary.scss';
 
 const InventoryByLibrary = () => {
-  const [by, setby] = useState(null);
+  const [fullInventory, setFullInventory] = useState([]);
+  const { allLibraries } = useSelector((state) => state.allLibraries);
+  const { catalogue } = useSelector((state) => state.catalogue);
+  const { publisher } = useSelector((state) => state.user.userData);
+  const dispatch = useDispatch();
+  const publisherData = useSelector((state) => state.publisher.publisher);
+  const userToken = localStorage.getItem('login-token');
 
-  const handleClick = (event) => {
-    setby(event.target.name);
-  };
+  useEffect(() => {
+    dispatch(getBooksByFilter({ publisher, userToken }));
+  }, [publisher, userToken]);
+
+  useEffect(() => {
+    if (publisher) {
+      dispatch(getLibrariesByPublisher({ publisher, userToken }));
+    }
+  }, [publisher, userToken]);
+
+  useEffect(() => {
+    if (Array.isArray(allLibraries)) {
+      const finalObject = allLibraries.map(({ _id, name }) => {
+        if (!catalogue || !Array.isArray(catalogue)) {
+          return null;
+        }
+        const books = catalogue.map(({ inventory, ...book }) => {
+          let copies = 0;
+          if (!inventory || !Array.isArray(inventory)) {
+            return null;
+          }
+          const libraryInventory = inventory.findIndex(
+            (library) => String(library.placeId) === String(_id),
+          );
+          if (libraryInventory >= 0) {
+            copies = inventory[libraryInventory].copies;
+          }
+          return ({
+            id: book._id,
+            title: book.title,
+            cover: book.cover,
+            copies,
+          });
+        });
+        return ({ id: _id, name, books });
+      });
+      const pub = { id: publisherData._id, name: publisherData.name };
+      if (catalogue && Array.isArray(catalogue)) {
+        const pubBooks = catalogue.map(({ inventory, ...book }) => {
+          let copies = 0;
+          if (!inventory || !Array.isArray(inventory)) {
+            return null;
+          }
+          const libraryInventory = inventory.findIndex(
+            (library) => String(library.placeId) === String(publisherData._id),
+          );
+          if (libraryInventory >= 0) {
+            copies = inventory[libraryInventory].copies;
+          }
+          return ({
+            id: book._id,
+            title: book.title,
+            cover: book.cover,
+            copies,
+          });
+        });
+        pub.books = pubBooks;
+        finalObject.unshift(pub);
+      }
+      setFullInventory(finalObject);
+    }
+  }, [allLibraries, catalogue, publisherData]);
 
   return (
-    <h3>Por librería</h3>
+    <section className="by-library">
+      <h3>Por librería</h3>
+      {fullInventory && Array.isArray(fullInventory)
+        ? fullInventory.map((library) => (
+          <article key={library.id}>
+            <h4>{library.name}</h4>
+            {library.books && Array.isArray(library.books) && (
+              <div>
+                {library.books.map((book) => (
+                  <>
+                    <figure className="by-library__book-fig">
+                      <img className="by-library__book-img" src={book.cover} alt={book.title} />
+                    </figure>
+                    <div key={book.id}>
+                      <p><b>{book.title}</b></p>
+                      <p>Cantidad de ejemplares: {book.copies}</p>
+                    </div>
+                  </>
+                ))}
+              </div>
+            )}
+            <hr />
+          </article>
+        ))
+        : null}
+    </section>
   );
 };
 export default InventoryByLibrary;
