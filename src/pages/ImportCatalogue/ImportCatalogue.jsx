a33a7e940547acb7929b9b61e6744dcc7b60cf8a
Juan Camilo Orjuela <orjuela9@gmail.com>
2023-08-15 18:12:12 -0500

    set basic structure for importing catalogue

---
+++ src/pages/ImportCatalogue/ImportCatalogue.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable no-unused-vars */
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
+import TopNav from '../../components/TopNav/TopNav';
+import ImportExcelForm from '../../feature/import/components/ImportInventoryForm/ImportInventoryForm';
+
+const ImportCatalogue = () => (
+  <div className="libraries">
+    <TopNav />
+    <main className="libraries__main-container">
+      <h2>Importa tu inventario</h2>
+      <div>
+        <FontAwesomeIcon icon={faExclamationTriangle} />
+        <p>
+          Al importar un inventario nuevo se remplazan todos los datos del inventario actual.
+          Procede solo si estas seguro de querer eliminar el inventario actual.
+        </p>
+      </div>
+      <div>
+        <ImportExcelForm />
+      </div>
+    </main>
+  </div>
+);
+
+export default ImportCatalogue;
