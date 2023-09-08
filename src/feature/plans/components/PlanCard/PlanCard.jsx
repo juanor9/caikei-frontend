d51e57da5597f16be06e8818b8c197224945baf6
Juan Camilo Orjuela <orjuela9@gmail.com>
2023-09-08 18:50:25 -0500

    add link for payment

--- src/feature/plans/components/PlanCard/PlanCard.jsx
+++ src/feature/plans/components/PlanCard/PlanCard.jsx
@@ -7,9 +7,24 @@ const PlanCard = ({ plan, cost, titles }) => {
     minimumFractionDigits: 0,
     maximumFractionDigits: 0,
   });
+  const planPaymentLink = {
+    10: 'https://www.mercadopago.com.co/subscriptions/checkout?preapproval_plan_id=2c9380848a6c207d018a75ba5ac9062a',
+    20: 'https://www.mercadopago.com.co/subscriptions/checkout?preapproval_plan_id=2c9380848a6c2082018a75bb234005f2',
+    30: 'https://www.mercadopago.com.co/subscriptions/checkout?preapproval_plan_id=2c9380848a6c2082018a75bbcd0c05f3',
+    40: 'https://www.mercadopago.com.co/subscriptions/checkout?preapproval_plan_id=2c9380848a6c2082018a75c10e8005f8',
+    60: 'https://www.mercadopago.com.co/subscriptions/checkout?preapproval_plan_id=2c9380848a6c204d018a75c1883c0654',
+    80: 'https://www.mercadopago.com.co/subscriptions/checkout?preapproval_plan_id=2c9380848a6c2082018a75c2211f05f9',
+    100: 'https://www.mercadopago.com.co/subscriptions/checkout?preapproval_plan_id=2c9380848a6c204d018a75c29cf50658',
+    200: 'https://www.mercadopago.com.co/subscriptions/checkout?preapproval_plan_id=2c9380848a6c207d018a75c34598062e',
+    400: 'https://www.mercadopago.com.co/subscriptions/checkout?preapproval_plan_id=2c9380848a6c204d018a75c3e354065a',
+  };
+  const PaymentLink = planPaymentLink[titles];
   return (
     <div>
       <p><b>{plan}</b></p>
+      {PaymentLink
+        ? <a href={PaymentLink}>Suscribirse</a>
+        : null}
       {cost > 0
         ? <p>{currencyCost}+IVA/mes</p>
         : <p>{currencyCost}</p>}
